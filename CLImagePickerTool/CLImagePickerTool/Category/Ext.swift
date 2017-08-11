e4ca03d11a384b127e9c212cbe02e221ba9e6144
陈亮陈亮 <chenliang@hd123.com>
2017-08-11 14:42:25 +0800

    移除无关文件

--- CLImagePickerTool/CLImagePickerTool/Category/Ext.swift
+++ CLImagePickerTool/CLImagePickerTool/Category/Ext.swift
@@ -13,24 +13,10 @@ extension UIImage {
     static func scaleImage(image: UIImage) -> UIImage {
         let picBili: CGFloat = image.size.width/image.size.height
 
-        // 画板高度
-        let boardH = KScreenHeight-50-44
         // 图片大小   UIScreen.main.scale屏幕密度，不加这个图片会不清晰
         UIGraphicsBeginImageContextWithOptions(CGSize(width:KScreenWidth,height:KScreenWidth/picBili), false, UIScreen.main.scale)
-        // 真正图片显示的位置
-        // 图片的宽高比
-        // 画板的宽高比
-        let boardBili: CGFloat = KScreenWidth/boardH
-        
-        // 如果图片太长，以高为准,否则以宽为准
-//        if picBili<=boardBili {
-//            image.draw(in: CGRect(x:0, y: 0, width: KScreenWidth, height: KScreenWidth/picBili))
-//        } else {
-//            image.draw(in: CGRect(x: 0, y:0.5*(boardH-KScreenWidth/picBili) , width: KScreenWidth, height: KScreenWidth/picBili))
-//        }
         
         image.draw(in: CGRect(x: 0, y:0 , width: KScreenWidth, height: KScreenWidth/picBili))
-
         
         let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
         
